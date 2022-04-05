4406227dda10b89b4a5f3fd2d0dec98d89a6d962
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2022-04-05 18:15:02 +0300

    muse-js
    muse-js install and first test

--- src/ui/Body.js
+++ src/ui/Body.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { MuseClient } from "muse-js";
 
 export default function Body() {
+  async function main() {
+    let client = new MuseClient();
+    await client.connect();
+    await client.start();
+    client.eegReadings.subscribe((reading) => {
+      console.log(reading);
+      setEeg1(reading);
+    });
+    client.telemetryData.subscribe((telemetry) => {
+      console.log(telemetry);
+      setEeg2(telemetry);
+    });
+    client.accelerometerData.subscribe((acceleration) => {
+      console.log(acceleration);
+      setEeg3(acceleration);
+    });
+  }
+
+  const [eeg1, setEeg1] = useState("will be here if conected");
+  const [eeg2, setEeg2] = useState("will be here if conected");
+  const [eeg3, setEeg3] = useState("will be here if conected");
+
   return (
     <div>
-      <p>Comming Soon!</p>
+      <h2
+        style={{
+          position: "absolute",
+          top: "60px",
+          left: "75px",
+          color: "black",
+        }}
+      >
+        Comming Soon!
+      </h2>
+      <button
+        style={{
+          position: "absolute",
+          width: "200px",
+          top: "170px",
+          left: "100px",
+          height: "100px",
+          fontSize: "26pt",
+          color: "red",
+        }}
+        onClick={async () => {
+          await main();
+        }}
+      >
+        connect to muse
+      </button>
+      <p>EEG data:</p>
+      <p>{eeg1}</p>
+      <p>{eeg2}</p>
+      <p>{eeg3}</p>
     </div>
   );
 }
