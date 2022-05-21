0c73414d153e7749f5bbfd135fdd65fa8a74470b
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2022-05-21 21:42:26 +0300

    Company Formation
    and laptop format

--- src/ui/Body.js
+++ src/ui/Body.js
@@ -3,21 +3,27 @@ import { MuseClient } from "muse-js";
 
 export default function Body() {
   async function main() {
-    let client = new MuseClient();
-    await client.connect();
-    await client.start();
-    client.eegReadings.subscribe((reading) => {
-      console.log(reading);
-      setEeg1(reading);
-    });
-    client.telemetryData.subscribe((telemetry) => {
-      console.log(telemetry);
-      setEeg2(telemetry);
-    });
-    client.accelerometerData.subscribe((acceleration) => {
-      console.log(acceleration);
-      setEeg3(acceleration);
-    });
+    try {
+      let client = new MuseClient();
+      await client.connect();
+      await client.start();
+
+      client.telemetryData.subscribe((telemetry) => {
+        console.log(telemetry);
+        setEeg1(telemetry);
+      });
+
+      client.eegReadings
+        .filter((r) => r.electrode === leftEyeChannel)
+        .subscribe((value) => {
+          setEeg2(value);
+        });
+
+      client.accelerometerData.subscribe((acceleration) => {
+        console.log(acceleration);
+        setEeg3(acceleration);
+      });
+    } catch (e) {}
   }
 
   const [eeg1, setEeg1] = useState("will be here if conected");
@@ -25,37 +31,42 @@ export default function Body() {
   const [eeg3, setEeg3] = useState("will be here if conected");
 
   return (
-    <div>
-      <h2
-        style={{
-          position: "absolute",
-          top: "60px",
-          left: "75px",
-          color: "black",
-        }}
-      >
-        Comming Soon!
-      </h2>
-      <button
-        style={{
-          position: "absolute",
-          width: "200px",
-          top: "170px",
-          left: "100px",
-          height: "100px",
-          fontSize: "26pt",
-          color: "red",
-        }}
-        onClick={async () => {
-          await main();
-        }}
-      >
-        connect to muse
-      </button>
-      <p>EEG data:</p>
-      <p>{eeg1}</p>
-      <p>{eeg2}</p>
-      <p>{eeg3}</p>
-    </div>
+    <>
+      {" "}
+      <div>
+        <h2
+          style={{
+            position: "absolute",
+            top: "60px",
+            left: "75px",
+            color: "black",
+          }}
+        >
+          Comming Soon!
+        </h2>
+        <button
+          style={{
+            position: "absolute",
+            width: "200px",
+            top: "170px",
+            left: "100px",
+            height: "100px",
+            fontSize: "26pt",
+            color: "red",
+          }}
+          onClick={async () => {
+            //await main();
+            await main();
+          }}
+        >
+          {" "}
+          connect to muse
+        </button>
+        <p>EEG data:</p>
+        <p>{JSON.stringify(eeg1)}</p>
+        <p>{JSON.stringify(eeg2)}</p>
+        <p>{JSON.stringify(eeg3)}</p>
+      </div>
+    </>
   );
 }
